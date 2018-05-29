f60ac2069585322c76fcf62a7ee0c548e52154ba
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-05-29 09:17:09 -0300

    configuração do mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://dxaraujo:1211968Dxa@ds137740.mlab.com:37740/bolaomesa5')
+module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/bolaomesa5')
