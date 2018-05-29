74ca4b50342ef0f1f1ac3063a834fb790dd7d627
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-05-29 09:02:09 -0300

    correção do mongodb host

--- src/config/database.js
+++ src/config/database.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/bolao')
+module.exports = mongoose.connect('mongodb://dxaraujo:1211968Dxa@ds137740.mlab.com:37740/bolaomesa5')
