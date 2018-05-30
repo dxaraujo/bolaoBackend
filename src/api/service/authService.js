3a957dba4440428e8b63562b008d52e5a44ebb85
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-05-29 23:15:55 -0300

    listagem de usuários

--- src/api/service/authService.js
+++ src/api/service/authService.js
@@ -43,8 +43,8 @@ const login = (req, res) => {
 			console.log(err)
 		} else if (user && bcrypt.compareSync(password, user.password)) {
 			const token = jwt.sign(user.toJSON(), env.authSecret, { expiresIn: "1 day" })
-			const { name, username, avatar, palpites } = user
-			res.json({ name, username, avatar, palpites, token })
+			const { name, username, avatar, palpites, pontuacao } = user
+			res.json({ name, username, avatar, palpites, pontuacao, token })
 		} else {
 			return res.status(400).send({
 				errors: ['Usuário/Senha inválidos']
