3778f8e8da34626318145db98146460a4217cdbb
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-06-04 16:06:43 -0300

    alterações para processamento das pontuações

--- src/api/service/partidaService.js
+++ src/api/service/partidaService.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const moment = require('moment')
 const Partida = require('../model/partida')
+const Palpite = require('../model/palpite')
+const User = require('../model/user')
 const { respondOrErr, handlerError } = require('../../util/serviceUtils')
 
 const router = express.Router()
@@ -38,6 +40,15 @@ router.post('/', (req, res, next) => {
 	})
 })
 
+router.put('/:id/updateResultado', (req, res, next) => {
+	Partida.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, data) => {
+		if (data) {
+			atualizarPontuacao(data);
+		}
+		respondOrErr(res, next, 500, err, 200, { data })
+	})
+})
+
 router.put('/:id', (req, res, next) => {
 	Partida.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, data) => {
 		respondOrErr(res, next, 500, err, 200, { data })
@@ -51,5 +62,40 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use(handlerError)
+
+const atualizarPontuacao = partida => {
+	User.find({}, (err, users) => {
+		users.forEach(user => {
+			Palpite.findOne({ user: user._id, partida: partida_id }, (err, palpite) => {
+				const palpiteTimeVencedor = palpite.placarTimeA > palpite.placarTimeB ? 'A' : palpite.placarTimeB > palpite.placarTimeA ? 'B' : 'E'
+				const partidaTimeVencedor = partida.placarTimeA > partida.placarTimeB ? 'A' : partida.placarTimeB > partida.placarTimeA ? 'B' : 'E'
+				if (palpite.placarTimeA === partida.placarTimeA && palpite.placarTimeB === partida.placarTimeB) {
+					palpite.totalPontosObitidos = 5
+					palpite.placarCheio = true
+				} else if (palpiteTimeVencedor === partidaTimeVencedor) {
+					if (palpite.placarTimeA === partida.placarTimeA || palpite.placarTimeB === partida.placarTimeB) {
+						palpite.totalPontosObitidos = 3
+						palpite.placarTimeVencedorComGol = true
+					} else {
+						palpite.totalPontosObitidos = 2
+						palpite.placarTimeVencedor = true
+					}
+				} else if (palpite.placarTimeA === partida.placarTimeA || palpite.placarTimeB === partida.placarTimeB) {
+					palpite.totalPontosObitidos = 1
+					palpite.placarGol = true
+				}
+				user.totalAcumulado = user.totalAcumulado + palpite.totalPontosObitidos
+				Palpite.findByIdAndUpdate(palpite._id, palpite, (err, data) => {
+				})
+			})
+		})
+		users = users.order((u1, u2) => u1.totalAcumulado > u2.totalAcumulado)
+		for (let i = 0; i < users.length; i++) {
+			users[i].classificacao = i + 1
+			User.findByIdAndUpdate(users[i]._id, users[i], (err, data) => {
+			})
+		}
+	})
+}
 
 exports = module.exports = router
