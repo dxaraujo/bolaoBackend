241cb9cd36eadaa016ad36c465e49f78cc14489a
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-06-16 19:52:26 -0300

    Atualização automática de resultados

---
+++ src/api/service/resultadoService.js
@@ -0,0 +1,135 @@
+const Partida = require('../model/partida')
+const Palpite = require('../model/palpite')
+const User = require('../model/user')
+const { asyncForEach } = require('../../util/serviceUtils')
+
+const atualizarResultados = async (partidaId, placares) => {
+	const newPartida = await Partida.findByIdAndUpdate(partidaId, placares, { new: true })
+	const partidas = await Partida.find({}).sort({ order: 'asc' })
+	const users = await User.find({})
+	let mapPalpites = []
+
+	// Atualizado o total de ponto acumulados
+	await asyncForEach(users, async user => {
+		let palpites = await Palpite.find({ user: user._id }).sort({ 'partida.order': 'asc' })
+		mapPalpites[user._id] = autalizarTotalAcumulado(partidas, palpites)
+	})
+
+	// Atualizado a classificação dos usuários
+	await asyncForEach(partidas, async partida => {
+		if (partida.placarTimeA >= 0 && partida.placarTimeB >= 0) {
+			let palpites = users.map(user => findPalpite(mapPalpites[user._id], partida))
+			palpites = await classificarPalpites(palpites)
+		}
+	})
+
+	// Atualizado os dados dos usuários
+	await asyncForEach(users, async user => {
+		if (mapPalpites[user._id]) {
+			let classificacao = null
+			let totalAcumulado = null
+			const palpites = mapPalpites[user._id]
+			for (let i = palpites.length - 1; i >= 0; i--) {
+				const palpite = palpites[i];
+				if (palpite.totalAcumulado >= 0 && palpite.classificacao >= 0) {
+					classificacao = palpite.classificacao
+					totalAcumulado = palpite.totalAcumulado
+					break
+				}
+			}
+			if (classificacao !== null && totalAcumulado !== null) {
+				user = await User.findByIdAndUpdate(user._id, { classificacao, totalAcumulado }, { new: true })
+			}
+		}
+	})
+	return newPartida
+}
+
+const findPalpite = (palpites, partida) => {
+	return palpites.find(palpite => {
+		return palpite.partida.fase === partida.fase &&
+			palpite.partida.grupo === partida.grupo &&
+			palpite.partida.rodada === partida.rodada &&
+			palpite.partida.timeA.nome === partida.timeA.nome &&
+			palpite.partida.timeB.nome === partida.timeB.nome
+	})
+}
+
+const autalizarTotalAcumulado = (partidas, palpites) => {
+	let totalAcumulado = 0
+	partidas.forEach(partida => {
+		if (partida.placarTimeA >= 0 && partida.placarTimeB >= 0) {
+			let palpite = findPalpite(palpites, partida)
+			if (palpite != null) {
+				palpite = calcularPontuacaoPalpite(palpite, partida)
+				totalAcumulado += palpite.totalPontosObitidos
+				palpite.totalAcumulado = totalAcumulado
+			}
+		}
+	})
+	return palpites
+}
+
+const classificarPalpites = async (palpites) => {
+	let cla = 1
+	let mesmoplacar = 1
+	palpites = palpites.filter(palpite => palpite)
+	palpites = palpites.sort((p1, p2) => p2.totalAcumulado - p1.totalAcumulado)
+	for (let i = 0; i < palpites.length; i++) {
+		if (i > 0) {
+			if (palpites[i].totalAcumulado === palpites[i - 1].totalAcumulado) {
+				cla = palpites[i - 1].classificacao
+				mesmoplacar += 1
+			} else {
+				cla = cla + mesmoplacar
+				mesmoplacar = 1
+			}
+		}
+		palpites[i].classificacao = cla
+		palpites[i] = await Palpite.findByIdAndUpdate(palpites[i]._id, palpites[i], { new: true })
+	}
+	return palpites
+}
+
+const calcularPontuacaoPalpite = (palpite, partida) => {
+	if (palpite.placarTimeA >= 0 && palpite.placarTimeB >= 0) {
+		const palpiteTimeVencedor = palpite.placarTimeA > palpite.placarTimeB ? 'A' : palpite.placarTimeB > palpite.placarTimeA ? 'B' : 'E'
+		const partidaTimeVencedor = partida.placarTimeA > partida.placarTimeB ? 'A' : partida.placarTimeB > partida.placarTimeA ? 'B' : 'E'
+		if (palpite.placarTimeA === partida.placarTimeA && palpite.placarTimeB === partida.placarTimeB) {
+			palpite.totalPontosObitidos = 5
+			palpite.placarCheio = true
+			palpite.placarTimeVencedorComGol = false
+			palpite.placarTimeVencedor = false
+			palpite.placarGol = false
+		} else if (palpiteTimeVencedor === partidaTimeVencedor) {
+			if (palpite.placarTimeA === partida.placarTimeA || palpite.placarTimeB === partida.placarTimeB) {
+				palpite.totalPontosObitidos = 3
+				palpite.placarCheio = false
+				palpite.placarTimeVencedorComGol = true
+				palpite.placarTimeVencedor = false
+				palpite.placarGol = false
+			} else {
+				palpite.totalPontosObitidos = 2
+				palpite.placarCheio = false
+				palpite.placarTimeVencedorComGol = false
+				palpite.placarTimeVencedor = true
+				palpite.placarGol = false
+			}
+		} else if (palpite.placarTimeA === partida.placarTimeA || palpite.placarTimeB === partida.placarTimeB) {
+			palpite.totalPontosObitidos = 1
+			palpite.placarCheio = false
+			palpite.placarTimeVencedorComGol = false
+			palpite.placarTimeVencedor = false
+			palpite.placarGol = true
+		} else {
+			palpite.totalPontosObitidos = 0
+			palpite.placarCheio = false
+			palpite.placarTimeVencedorComGol = false
+			palpite.placarTimeVencedor = false
+			palpite.placarGol = false
+		}
+	}
+	return palpite
+}
+
+exports = module.exports = atualizarResultados
---
+++ src/schedule.js
@@ -0,0 +1,51 @@
+const schedule = require('node-schedule')
+const moment = require('moment')
+const { JSDOM } = require('jsdom')
+const Partida = require('./api/model/partida')
+const atualizarResultados = require('./api/service/resultadoService')
+
+schedule.scheduleJob('*/5 9-17 * * *', () => {
+	console.log('chegou aqui')
+	JSDOM.fromURL('https://globoesporte.globo.com/placar-ge/hoje/jogos.ghtml').then(async dom => {
+		const date = moment().subtract(3, 'hours').toDate()
+		const partidas = await Partida.find({ data: { $lt: date } }).sort({ order: 'asc' })
+		const doc = dom.window.document;
+		const jogos = doc.getElementsByClassName('card-jogo')
+		for (let i = 0; i < jogos.length; i++) {
+			const jogo = jogos.item(i)
+			const nomeJogo = jogo.getElementsByClassName('titulo').item(0).firstElementChild.innerHTML
+			const horarioJogo = jogo.getElementsByClassName('titulo').item(0).lastElementChild.getAttribute('datetime')
+			if (nomeJogo == 'Copa do Mundo da FIFA™') {
+				const timeA = jogo.getElementsByClassName('mandante').item(0).firstElementChild.getAttribute('title')
+				const timeB = jogo.getElementsByClassName('visitante').item(0).lastElementChild.getAttribute('title')
+				const resultado = jogo.getElementsByClassName('resultado').item(0)
+				if (resultado.childElementCount > 1) {
+					const placarTimeA = resultado.getElementsByClassName('placar-mandante').item(0).innerHTML
+					const placarTimeB = resultado.getElementsByClassName('placar-visitante').item(0).innerHTML
+					if (placarTimeA >= 0 && placarTimeB >= 0) {
+						const partida = partidas.find(partida => {
+							return partida.timeA.nome == timeA &&
+									partida.timeB.nome == timeB &&
+									moment(partida.data, 'YYYY-MM-DDThh:mm:ss').add(3, 'hours').isSame(moment(horarioJogo, 'YYYY-MM-DDThh:mm:ss'))
+						})
+						if (partida != null) {
+							console.log('achou partida')
+							console.log('Partida placarTimeA: ', partida.placarTimeA)
+							console.log('Partida placarTimeB: ', partida.placarTimeB)
+							console.log('PlacarTimeA: ', placarTimeA)
+							console.log('PlacarTimeB: ', placarTimeB)
+							if (partida.placarTimeA != placarTimeA || partida.placarTimeB != placarTimeB) {
+								console.log('achou partida com placar desatualizado')
+								const newPartida = await atualizarResultados(partida._id, { placarTimeA, placarTimeB })
+								console.log(`Partida: ${newPartida._id} - ${newPartida.timeA.nome} ${newPartida.placarTimeA} x ${newPartida.placarTimeB} ${newPartida.timeB.nome}`)
+							}
+						}
+					}	
+				}
+			}
+		}
+		console.log('finalizou a atualização dos resultados')
+	}).catch(err => {
+		console.log(err)
+	})
+});
