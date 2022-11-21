b249d0047a4e5b76b680396f0423767a31a75c95
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2022-11-21 00:25:10 -0300

    Ajuste na consultada dos resultados

--- src/schedule.js
+++ src/schedule.js
@@ -4,9 +4,33 @@ const moment = require('moment')
 const Partida = require('./api/model/partida')
 const atualizarResultados = require('./api/service/resultadoService')
 
-const URL = 'https://www.uol.com.br/service.htm?type=projects/world-cup-2022/calendar/calendar-api-2022&v=6&args={"search":{"params":{"season":2022,"setDate":false}}}'
+const URL = 'https://www.estadao.com.br/pf/api/v3/content/fetch/esportes?query={"params":{"mode":"api"},"requestUri":"https://estadao.com.br/esportes/futebol/partidas/copa-do-mundo/2022"}&d=302&_website=estadao'
 
-schedule.scheduleJob('*/5 7-19 * * *', async () => {
+const obterJogos = (data) => {
+	let result = []
+	if (data && data.fases) {
+		const fases = data.fases
+		for (let i = 0; i < fases.length; i++) {
+			const fase = fases[i]
+			if (fase && fase.grupos) {
+				const grupos = fase.grupos
+				for (let j = 0; j < grupos.length; j++) {
+					const grupo = grupos[j]
+					if (grupo && grupo.jogos) {
+						const jogos = grupo.jogos
+						for (let k = 0; k < jogos.length; k++) {
+							result.push(jogos[k])
+						}
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+// schedule.scheduleJob('*/5 7-19 * * *', async () => {
+schedule.scheduleJob('* * * * *', async () => {
 	console.log('iniciou a atualização dos resultados')
 	try {
 		const response = await fetch(URL)
@@ -15,40 +39,23 @@ schedule.scheduleJob('*/5 7-19 * * *', async () => {
 		console.log('Data atual: ', date)
 		const partidas = await Partida.find({ data: { $lt: date } }).sort({ order: 'asc' })
 		// console.log(`Encontrou ${partidas.length} partidas`)
-		const jogos = data.matches
-		const times = data.teams
-		times.map(time => {
-			if (time.abbr === 'QAT') {
-				time.abbr = 'CAT'
-			} else if (time.abbr === 'SAU') {
-				time.abbr = 'ARA'
-			} else if (time.abbr === 'COS') {
-				time.abbr = 'CRC'
-			} else if (time.abbr === 'CDS') {
-				time.abbr = 'COR'
-			} else if (time.abbr === 'SBR') {
-				time.abbr = 'SER'
-			}
-		})
+		const jogos = obterJogos(data)
 		// console.log(`Encontrou ${jogos.length} jogos`)
 		for (let i = 0; i < jogos.length; i++) {
-			const horarioJogo = jogos[i].info.dateString
-			const timeA = jogos[i].team1
-			const timeB = jogos[i].team2
-			const siglaTimeA = times.find(time => time.id === timeA.id).abbr
-			const siglaTimeB = times.find(time => time.id === timeB.id).abbr
-			const placarTimeA = timeA.score
-			const placarTimeB = timeB.score
+			const horarioJogo = jogos[i].data
+			const siglaTimeA = jogos[i].time_1 ? jogos[i].time_1.sigla : undefined
+			const siglaTimeB = jogos[i].time_2 ? jogos[i].time_2.sigla : undefined
+			const placarTimeA = jogos[i].time_1 ? jogos[i].time_1.gols : undefined
+			const placarTimeB = jogos[i].time_2 ? jogos[i].time_2.gols : undefined
 			if (placarTimeA && placarTimeA >= 0 && placarTimeB && placarTimeB >= 0) {
 				const partida = partidas.find(partida => {
 					return partida.timeA && partida.timeA.sigla == siglaTimeA 
 						&& partida.timeB && partida.timeB.sigla == siglaTimeB
-						&& moment(horarioJogo, 'YYYY-MM-DD hh:mm').subtract(3, 'hours').isSame(moment(partida.data, 'YYYY-MM-DDThh:mm:ss'))
+						&& moment(horarioJogo, 'MMM DD YYYY hh:mm:ss:SSSA').subtract(3, 'hours').isSame(moment(partida.data, 'YYYY-MM-DDThh:mm:ss'))
 				})
 				if (partida != null) {
-					// console.log(`Achou partida ${partida.timeA.sigla} ${partida.placarTimeA | ' '} x ${partida.placarTimeB | ' '} ${partida.timeB.sigla}`)
 					if (partida.placarTimeA != placarTimeA || partida.placarTimeB != placarTimeB) {
-						// console.log(`Partida: ${partida.timeA.sigla} ${partida.placarTimeA | ' '} x ${partida.placarTimeB | ' '} ${partida.timeB.sigla} com placar desatualizado`)
+						console.log(`Partida: ${partida.timeA.sigla} ${partida.placarTimeA | ' '} x ${partida.placarTimeB | ' '} ${partida.timeB.sigla} com placar desatualizado`)
 						const newPartida = await atualizarResultados(partida._id, { placarTimeA, placarTimeB })
 						console.log(`Partida: ${newPartida.timeA.sigla} ${newPartida.placarTimeA} x ${newPartida.placarTimeB} ${newPartida.timeB.sigla} atualizada`)
 					}
