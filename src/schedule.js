9020b704680fc2f09e8866b5ade89db155310dd9
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2022-11-27 11:00:59 -0300

    Correção do horário do jogo

--- src/schedule.js
+++ src/schedule.js
@@ -25,12 +25,12 @@ schedule.gracefulShutdown().then(() => {
 				if (placarTimeA && placarTimeA >= 0 && placarTimeB && placarTimeB >= 0) {
 					console.log(`Achou jogo com placar ${siglaTimeA} ${placarTimeA} x ${placarTimeB} ${siglaTimeB}`)
 					const partida = partidas.find(partida => {
-						console.log(moment(horarioJogo))
-						console.log(moment(partida.data, 'YYYY-MM-DDThh:mm:ss').add(3, 'hours'))
+						console.log(moment(horarioJogo).subtract(3, 'hours'))
+						console.log(moment(partida.data, 'YYYY-MM-DDThh:mm:ss'))
 						console.log(moment(horarioJogo).isSame(moment(partida.data, 'YYYY-MM-DDThh:mm:ss')))
 						return partida.timeA && partida.timeA.sigla == siglaTimeA 
 							&& partida.timeB && partida.timeB.sigla == siglaTimeB
-							&& moment(horarioJogo).isSame(moment(partida.data, 'YYYY-MM-DDThh:mm:ss').add(3, 'hours'))
+							&& moment(horarioJogo).subtract(3, 'hours').isSame(moment(partida.data, 'YYYY-MM-DDThh:mm:ss'))
 					})
 					if (partida != null) {
 						console.log(`Achou partida ${partida.timeA.sigla} ${partida.placarTimeA | ' '} x ${partida.placarTimeB | ' '} ${partida.timeB.sigla}`)
